03b52a2bd02214b9f0f5b77d0539b6bf35310099
Mihaidono <amihai2001@gmail.com>
2022-11-17 09:38:23 +0200

    managed to generate random centroids and calculate the distance between the points and the centroids in order to get the min distance

---
+++ Laborator6/main.py
@@ -0,0 +1,35 @@
+import random
+
+import numpy as np
+
+number_of_clusters = 3
+# usually it needs to be calculated but for this
+# example I already knew how many clusters there are
+
+node_array = [[45, 85], [50, 43], [40, 80], [55, 42], [200, 43], [48, 40], [195, 41], [43, 87], [190, 40]]
+node_array = np.array(node_array)
+
+# initialized the nodes to be clustered
+centroid_array = []
+last_rand = -1
+while True:
+    new_rand = random.randint(0, len(node_array) - 1)
+    if last_rand is not new_rand:
+        centroid_array.append(node_array[new_rand])
+        last_rand = new_rand
+    if len(centroid_array) == number_of_clusters:
+        break
+
+countIsSame = 0
+while countIsSame < 3:
+    clusters = {}
+    for i in range(0, len(node_array)):
+        print("Centroids chosen for this iteration: ", centroid_array)
+        cluster_value = []
+        for j in range(0, number_of_clusters):
+            cluster_value.append(
+                abs(centroid_array[j][0] - node_array[i][0]) + abs(centroid_array[j][1] - node_array[i][1]))
+            # finding out who the closest centroid is by storing the distance of the point from all the centroids
+
+        clusters[cluster_value.index(np.min(cluster_value))] = node_array[i]
+        print(clusters)

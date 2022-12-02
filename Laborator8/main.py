f447296486b0f072becc703e8db22ec439bb1fe8
Mihaidono <amihai2001@gmail.com>
2022-12-02 12:50:36 +0200

    this lab i skipped for some reason and now I'm solving it, added the values array and response for the Perceptron Learning Rule

---
+++ Laborator8/main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+weight_array = np.array([0, 1, 0])
+
+firstV_array = np.array([2, 1, -1])
+firstV_array_response = -1
+
+secondV_array = np.array([0, -1, -1])
+secondV_array_response = 1

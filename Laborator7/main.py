d0ff59c954c1c7ce555fcc8d04efd0bdec871b91
Mihaidono <amihai2001@gmail.com>
2022-12-07 11:01:49 +0200

    added array to store results for the selection functions which will be sorted for the cutting process

--- Laborator7/main.py
+++ Laborator7/main.py
@@ -17,6 +17,21 @@ def formPopulationArray(length):
     return ppl_array
 
 
+def GetSelectionProbabilityArray(ppl_array):
+    sp_array = []
+    for i in range(0, len(ppl_array)):
+        aux_array = ppl_array[i]
+        aux_sum = 0
+        aux_prod = 1
+        for j in range(0, len(aux_array)):
+            if aux_array[j] == 0:
+                aux_sum += aux_array[j]
+            else:
+                aux_prod *= aux_array[j]
+        sp_array.append(objective_function(aux_sum, aux_prod))
+    return sp_array
+
+
 # for this card game we want to split the deck into 2 and sum the first half and make it as close as
 # this input variable:
 print("The values must be !Integers!")
@@ -40,3 +55,5 @@ def objective_function(sum_array, mult_array):
 population_size = 50  # change this to change the number of elements in the array
 
 population_array = np.array(formPopulationArray(population_size))
+
+selection_prob_array = np.array(GetSelectionProbabilityArray(population_array))

8a71fefca0416e7dcacbe60748d6e7c51d10e52d
Mihaidono <amihai2001@gmail.com>
2022-12-11 12:44:38 +0200

    started implementing lab9: the delta rule

---
+++ Laborator9/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+error_value = 0  # initialized error value which will be compared with the max allowed error
+
+max_error_value = 0.0001  # change this to modify the accuracy of the output
+
+

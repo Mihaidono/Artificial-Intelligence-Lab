32d78b8c64bba83e1304c5dcaf2dea8554c361ac
Mihaidono <amihai2001@gmail.com>
2022-12-14 11:06:25 +0200

    finished the homework it properly calculates the 2 unknown variables from the linear regression

--- Laborator10/main.py
+++ Laborator10/main.py
@@ -10,9 +10,27 @@ def GetDataFromFile():
         csvreader = csv.reader(file)
         next(csvreader)
         for row in csvreader:
-            obs_values_x.append(row[0])
-            obs_values_y.append(row[1])
+            obs_values_x.append(float(row[0]))
+            obs_values_y.append(float(row[1]))
     return obs_values_x, obs_values_y
+
+
+def GetFunctionForW1(w1, w2):
+    sum = 0.0
+    for i in range(0, len(x_observed_values)):
+        sum += (y_observed_values[i] - (w1 * x_observed_values[i] + w2)) * x_observed_values[i]
+
+    ans = (-1 / len(x_observed_values)) * sum
+    return ans
+
+
+def GetFunctionForW2(w1, w2):
+    sum = 0
+    for i in range(0, len(x_observed_values)):
+        sum += (y_observed_values[i] - (w1 * x_observed_values[i] + w2))
+
+    ans = (-1 / len(x_observed_values)) * sum
+    return ans
 
 
 x_observed_values, y_observed_values = GetDataFromFile()
@@ -22,6 +40,7 @@ print("x values from csv:", x_observed_values, "\ny values from csv:", y_observe
 learning_step = 0.01
 
 threshold_value = 0.0001
+# 0.0001 learning step and 0.0000001 threshold value get really close to the wanted result
 # both the learning step and threshold value are important factors for the accuracy of the results
 # the lower the learning step the closer to a good result it can come
 # the lower the threshold the less of an error between the wanted answer and actual answer will be
@@ -34,15 +53,16 @@ next_value_w2 = 0
 print("For the error function E(w1,w2)=(1/2n)*(SUM1->n(yi-(w1xi+w2))^2)")
 number_of_iterations = 1
 while True:
-    print("Iteration ", number_of_iterations)
     number_of_iterations += 1
 
-    next_value_w1 = initial_value_w1 - learning_step * ( )
+    next_value_w1 = initial_value_w1 - learning_step * (GetFunctionForW1(initial_value_w1, initial_value_w2))
 
-    next_value_w2 = initial_value_w2 - learning_step * ( )
+    next_value_w2 = initial_value_w2 - learning_step * (GetFunctionForW2(initial_value_w1, initial_value_w2))
 
     if abs(next_value_w1 - initial_value_w1) < threshold_value and abs(
             next_value_w2 - initial_value_w2) < threshold_value:
         break
     initial_value_w1 = next_value_w1
     initial_value_w2 = next_value_w2
+
+print(f"Final values after {number_of_iterations} iterations:\nw1 = {next_value_w1}\nw2 = {next_value_w2}")

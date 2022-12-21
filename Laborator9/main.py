b1992ed525b4af3e062ea070cf50bedc562ba556
Mihaidono <amihai2001@gmail.com>
2022-12-21 22:45:46 +0200

    calculates next weight array and outputs

--- Laborator9/main.py
+++ Laborator9/main.py
@@ -7,8 +7,13 @@ def normalizeDataSet(dataset):
 
 
 def GetOutput(dataset, weight):
+    output_array = []
     for i in range(0, len(dataset)):
-        break
+        input_sum = 0
+        for j in range(0, len(dataset[0])):
+            input_sum += dataset[i, j] * weight[i, j]
+        output_array.append(bipolar_function(input_sum))
+    return np.array(output_array)
 
 
 def bipolar_function(x):
@@ -23,7 +28,19 @@ def initWeightArray(dataset_variable_number, dataset_size):
         while len(mini_array) < dataset_variable_number:
             mini_array.append(r_array[random.randint(0, len(r_array) - 1)])
         aux_array.append(mini_array)
-    return aux_array
+    return np.array(aux_array)
+
+
+def calculateNewWeightArray():
+    next_weights = []
+    for i in range(0, len(dataset_array)):
+        node_weight = []
+        for j in range(0, len(dataset_array[0])):
+            node_weight.append(weight_array[i][j] + learning_step * (wanted_output[i][j] - output[i]) *
+                               (1 - wanted_output[i][j] ** 2) * dataset_array[i][j])
+            # calculated using the formula in the documentation
+        next_weights.append(node_weight)
+    return np.array(next_weights)
 
 
 error_value = 0  # initialized error value which will be compared with the max allowed error
@@ -36,21 +53,20 @@ dataset_array = np.array(
     [[45, 85], [50, 43], [40, 80], [55, 42], [200, 43], [48, 40], [195, 41],
      [43, 87], [190, 40]])
 
-weight_array = np.array(initWeightArray(len(dataset_array[1]), len(dataset_array)))
-
 wanted_output = np.array(
     [[1, -1, -1], [-1, 1, -1], [1, -1, -1], [-1, 1, -1], [-1, -1, 1], [-1, 1, -1], [-1, -1, 1], [1, -1, -1],
      [-1, -1, 1]])
-
-output = np.array([])
-print("Data set:\n", dataset_array)
-print("Weights set:\n", weight_array)
-print("Wanted outputs are:\n", wanted_output)
 
 # normalizing data set
 dataset_array = normalizeDataSet(dataset_array)
 dataset_array = np.hstack((dataset_array, np.full((len(dataset_array), 1), -1)))  # added the column of -1's
 print("Normalized data set :\n", dataset_array)
 
+weight_array = initWeightArray(len(dataset_array[1]), len(dataset_array))
+print("Weights set:\n", weight_array)
+print("Wanted outputs are:\n", wanted_output)
+
 while True:
+    output = GetOutput(dataset_array, weight_array)
+    next_weight_array = calculateNewWeightArray()
     break
